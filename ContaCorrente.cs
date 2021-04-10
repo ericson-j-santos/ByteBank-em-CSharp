13823ed5189405614d2bb1fab4815f9c33a2aed0
ericsontakay1988 <60470500+ericsontakay1988@users.noreply.github.com>
2021-04-10 01:01:38 -0300

    Arquivo 04-ByteBank

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -6,5 +6,37 @@ public class ContaCorrente
 	public string titular;
 	public int agencia;
 	public int numero;
-	public double saldo;
+	public double saldo = 100;
+
+	public bool Sacar(double valor)
+	{
+		if (this.saldo < valor)
+		{
+			return false;
+		}
+		else
+		{
+			this.saldo -= valor;
+			return true;
+		}
+	}
+
+	public void Depositar(double valor)
+	{
+		this.saldo += valor;
+	}
+
+	public bool Transferir(double valor, ContaCorrente contaDestino)
+	{
+		if (this.saldo < valor)
+		{
+			return false;
+		}
+		else
+		{
+			this.saldo -= valor;
+			contaDestino.Depositar(valor);
+			return true;
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -1,30 +1,43 @@
 ﻿using System;
 
-namespace _03_ByteBank
+namespace _04_ByteBank
 {
 	class Program
 	{
 		static void Main(string[] args)
 		{
+			ContaCorrente contaDoBruno = new ContaCorrente();
+
+			contaDoBruno.titular = "Bruno";
+
+			Console.WriteLine(contaDoBruno.saldo);
+			contaDoBruno.Sacar(50);
+			Console.WriteLine(contaDoBruno.saldo);
+
+		    contaDoBruno.Depositar(500);
+			Console.WriteLine(contaDoBruno.saldo);
+
+
 			ContaCorrente contaDaGabriela = new ContaCorrente();
+
 			contaDaGabriela.titular = "Gabriela";
-			contaDaGabriela.agencia = 863;
-			contaDaGabriela.numero = 863452;
 
-			ContaCorrente contaDaGabrielaCosta = new ContaCorrente();
-			contaDaGabriela.titular = "Gabriela";
-			contaDaGabriela.agencia = 863;
-			contaDaGabriela.numero = 863452;
 
-			Console.WriteLine(contaDaGabrielaCosta == contaDaGabriela);
+			Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+			Console.WriteLine("Saldo do Gabriela: " + contaDaGabriela.saldo);
 
-			contaDaGabriela.saldo = 300;
-			Console.WriteLine(contaDaGabriela.saldo);
-			Console.WriteLine(contaDaGabrielaCosta.saldo);
+			bool resultadoTransferencia = contaDoBruno.Transferir(200, contaDaGabriela);
 
-			contaDaGabriela.saldo -= 100;
+			Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+			Console.WriteLine("Saldo do Gabriela: " + contaDaGabriela.saldo);
+
+
+
+
+
 
 			Console.ReadLine();
+
 		}
 	}
 }
